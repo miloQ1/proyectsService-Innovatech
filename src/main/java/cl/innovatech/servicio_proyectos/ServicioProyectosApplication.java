eed36ffa761e448d35bb2aaef1f56408dd87b72f
miloQ1 <ca.queupil@duocuc.cl>
2026-04-04 18:55:19 -0300

    primer commit - subida proyecto

---
+++ src/main/java/cl/innovatech/servicio_proyectos/ServicioProyectosApplication.java
@@ -0,0 +1,13 @@
+package cl.innovatech.servicio_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/innovatech/servicio_proyectos/ServicioProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.innovatech.servicio_proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
